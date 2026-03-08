4febb69226d9e1bce7fa4ec4c9c1fdd4fcdd468d
nttg8100 <nttg8100@gmail>
2026-03-09 01:39:48 +0700

    feat: benchmark on simple workflow

--- benchmark/pipelines/nf-germline-short-read-variant-calling/run_pipeline.sh
+++ benchmark/pipelines/nf-germline-short-read-variant-calling/run_pipeline.sh
@@ -1,8 +1,28 @@
-scratch/data/nf-germline-short-read-variant-calling
-nextflow run main.nf \
-    -profile docker -c /scratch/data/nf-germline-short-read-variant-calling/benchmark/pipelines/nf-germline-short-read-variant-calling/customized_labels.config \
+# cloning repo
+git clone https://github.com/nttg8100/nf-germline-short-read-variant-calling.git -b 0.4.0
+
+# Freebayes recommendation: 
+# 1. fastp: quality control
+# 2. bwa-mem2: read alignment  
+# 3. sambamba: PCR remove duplicate
+# 4. freebayes: Variant calling
+nextflow run nf-germline-short-read-variant-calling \
+    -profile docker -c customized_labels.config \
     --genome GRCh38 \
-    --input /scratch/data/nf-germline-short-read-variant-calling/benchmark/pipelines/nf-germline-short-read-variant-calling/input.csv \
+    --input input.csv \
+    --variant_caller "freebayes" \
+    --preprocessor "sambamba" \
+    --output variant_calling
+
+
+# Deepvariant recommendation: 
+# 1. fastp: quality control
+# 2. bwa-mem2: read alignment  
+# 3. deepvariant: variant calling 
+nextflow run nf-germline-short-read-variant-calling \
+    -profile docker -c customized_labels.config \
+    --genome GRCh38 \
+    --input input.csv \
     --variant_caller "freebayes" \
     --preprocessor "sambamba" \
     --output variant_calling
